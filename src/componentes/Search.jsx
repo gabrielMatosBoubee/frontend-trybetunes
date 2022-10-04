33906db08d61be50ff22de04b0da45bb0b71c3cf
petromamboubee <gm.boubee@gmail.com>
2022-10-03 21:46:11 -0300

    terminei o requisito 6

--- src/componentes/Search.jsx
+++ src/componentes/Search.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import Header from './Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Carregando from './Carregando';
+import { Link } from 'react-router-dom';
 
 class Search extends React.Component {
   constructor() {
@@ -7,6 +10,11 @@ class Search extends React.Component {
     this.state = {
       nome: '',
       disabled: true,
+      Button: true,
+      obj: {},
+      nomeSalvo: false,
+      primeiroButton: false,
+      nomeDigitado: '',
     };
   }
 
@@ -24,26 +32,68 @@ class Search extends React.Component {
     }
   };
 
+  onClickButton = async () => {
+    const { nome } = this.state;
+    this.setState({ Button: false });
+    const resultado = await searchAlbumsAPI(nome);
+    this.setState((prevState) => ({
+      nomeDigitado: prevState.nome,
+      nomeSalvo: true,
+      primeiroButton: true,
+      nome: '',
+      Button: true,
+      obj: resultado }));
+  };
+
   render() {
-    const { nome, disabled } = this.state;
+    const {
+      nome,
+      disabled,
+      Button,
+      obj,
+      nomeSalvo,
+      primeiroButton, nomeDigitado } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
         <input
+          id="id"
           type="text"
           data-testid="search-artist-input"
           name="nome"
           value={ nome }
           onChange={ this.onInputChange }
         />
-        <button
-          type="button"
-          disabled={ disabled }
-          data-testid="search-artist-button"
-        >
-          Pesquisar
-
-        </button>
+        {Button ? (
+          <button
+            type="button"
+            disabled={ disabled }
+            data-testid="search-artist-button"
+            onClick={ () => this.onClickButton() }
+          >
+            Pesquisar
+          </button>
+        ) : <Carregando />}
+        {primeiroButton && (obj.length === 0
+          ? <p>Nenhum álbum foi encontrado</p>
+          : (
+            <p>
+              Resultado de álbuns de:
+              {nomeDigitado}
+            </p>
+          ))}
+        {nomeSalvo && obj.map((ele) => (
+          <div key={ `div ${ele.collectionName}` }>
+            <p>{ele.artistName}</p>
+            <p>{ele.collectionName}</p>
+            <img src={ ele.artworkUrl100 } alt="img" />
+            <Link
+              to={ `/album/${ele.collectionId}` }
+              data-testid={ `link-to-album-${ele.collectionId}` }
+            />
+          </div>
+        ))}
+        ;
       </div>
     );
   }
